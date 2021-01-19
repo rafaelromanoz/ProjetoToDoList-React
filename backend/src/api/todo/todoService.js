fb31a0e97a41fe8e00ec58843abd3e339bf9bd20
Rafael de Oliveira Romano <rafa-kun@hotmail.com>
2021-01-19 17:50:35 -0300

    Criado toda a parte de backend e configuração do webpack

---
+++ backend/src/api/todo/todoService.js
@@ -0,0 +1,7 @@
+const todo = require('./todo')
+const Todo = require('./todo')
+
+Todo.methods(['get','post','put','delete'])
+Todo.updateOptions({new:true,runValidators:true})
+
+module.exports=Todo
