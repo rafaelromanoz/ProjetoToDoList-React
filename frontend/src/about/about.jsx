3d402888e237d1f310c9a3cb952cf4da6247066e
Rafael de Oliveira Romano <rafa-kun@hotmail.com>
2021-01-28 10:57:02 -0300

    Commit final

---
+++ frontend/src/about/about.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import pageHeader from '../template/pageHeader'
+
+export default props=>(
+    <div>
+        <PageHeader name='Sobre' small=' Nós'></PageHeader>
+        <h2>Nossa História</h2>
+        <p>Curso react redux todolist</p>
+        
+    </div>
+)
